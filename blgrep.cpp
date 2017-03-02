c6cce7af54480203c9df55d44a6603abc66895ca
Brendan Epstein <brendan@e420>
2017-03-01 20:26:22 -0800

    Added reverse, revcomp, and comp matching.

--- blgrep.cpp
+++ blgrep.cpp
@@ -3,14 +3,17 @@
  * a regex approach to all the matching, but I would like to implement
  * a biological matching approach too.
  *
+ * It refuses to read genbank files with a ".gb" extension unless they are
+ * piped in. I can't seem to convince SeqFileIn that genbank is the format.
+ *
  * TODO:
- *    - Output format based on input format
- *    - Handle input formats with quality scores
+ *    - Output format based on input format, or at least some choices
  *    - Ability to search within a range of sequence coordinates
  *    - Ability to spit out context around matches or the number of matches
  *    - Ability to split a fasta file into records using a GFF file -
  *      or maybe that should be a different tool?
- *    - Translate, complement, reverse complement
+ *    - Translate search
+ *    - Biologically-based search instead of regex
  *
  */
 
@@ -56,6 +59,9 @@ int main(int argc, char * argv[]) {
   TCLAP::SwitchArg case_sensitive_arg("I", "case-sensitive",
 				      "Do not ignore case in pattern and input; only for -S",
 				      cmd);
+  TCLAP::ValueArg<string> match_type_arg("M", "match-type",
+				     "Match type: f=fwd, r=rev, c=compl., R=revcomp; ignored for names",
+				     false, "f", "string", cmd);
   TCLAP::UnlabeledValueArg<string> regex_string_arg("PATTERN", "regex pattern",
 						    true, "",
 						    "regex", cmd, false);
@@ -66,6 +72,7 @@ int main(int argc, char * argv[]) {
   if(infiles.size() == 0) infiles.push_back("-");
   bool seq_regex = seq_regex_arg.getValue();
   bool inverted = invert_regex_arg.getValue();
+  string match_type = match_type_arg.getValue();
   if(ignore_case_arg.getValue() || (seq_regex && !case_sensitive_arg.getValue()))
     regex_flags |= regex::icase;
   regex regex_pattern(regex_string_arg.getValue(), regex_flags);
@@ -97,12 +104,60 @@ int main(int argc, char * argv[]) {
       try {
 
 	readRecord(id, seq, qual, seq_handle);
-      
+	
 	// Regex
 	if(seq_regex) {
-	  matched = regex_search(toCString(seq), regex_pattern,
-				 regex_match_flags);
+
+	  if(regex_search(match_type, regex("a"))) {
+	    match_type = "frcR";
+	  }
+
+	  // Due to some quirks of Seqan, I have to do a number of format
+	  // conversions, so this isn't as elegant as I would like.
+	  // Also this assumes DNA, not RNA, even though RNA could work
+	  // fine. Note that any type of sequence will work with regular
+	  // forward matching.
+	  matched = false;
+	  for(char c: match_type) {
+	    switch (c) {
+	    case 'f':
+	      {
+		CharString _seq = seq;
+		matched |= regex_search(toCString(_seq), regex_pattern,
+					regex_match_flags);
+		break;
+	      }
+	    case 'r':
+	      {
+		ModifiedString<CharString, ModReverse> rseq(seq);
+		CharString _seq(rseq);
+		matched |= regex_search(toCString(_seq), regex_pattern,
+					regex_match_flags);
+		break;
+	      }
+	    case 'c':
+	      {
+		Dna5String dseq(seq);
+		complement(dseq);
+		CharString _seq(dseq);
+		matched |= regex_search(toCString(_seq), regex_pattern,
+					regex_match_flags);
+		break;
+	      }
+	    case 'R':
+	      {
+		Dna5String dseq(seq);
+		reverseComplement(dseq);
+		CharString _seq(dseq);
+		matched |= regex_search(toCString(_seq), regex_pattern,
+					regex_match_flags);
+		break;
+	      }
+	    }
+	  } // End match_type loop
+
 	} else {
+	  
 	  matched = regex_search(toCString(id), regex_pattern,
 				 regex_match_flags);
 	} // End regex
@@ -131,5 +186,5 @@ int main(int argc, char * argv[]) {
   } else {
     return 1;
   }
-
+  
 }
